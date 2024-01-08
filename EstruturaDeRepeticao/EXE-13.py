2ec4215cbcff4763fc2065b23a39909a7ac19523
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-01-08 16:59:32 -0300

    Exercicio 13 de EstruturaDeRepeticao

---
+++ EstruturaDeRepeticao/EXE-13.py
@@ -0,0 +1,10 @@
+# 13 - Faça um programa que peça dois números, base e expoente, 
+# calcule e mostre o primeiro número elevado ao segundo número.
+# Não utilize a função de potência da linguagem.
+
+num1 = int(input('Informe o primeiro número: '))
+num2 = int(input('Informe o segundo número: '))
+
+resultado = num1 ** num2
+
+print(resultado)
