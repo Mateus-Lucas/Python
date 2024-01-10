fd10113105c0682de9b57188a96a6a9e1efa78cf
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-01-10 15:22:11 -0300

    Exercicio 15 de EstruturaDeRepeticao

---
+++ EstruturaDeRepeticao/EXE-15.py
@@ -0,0 +1,14 @@
+# 15 - A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... 
+# Faça um programa capaz de gerar a série até o n−ésimo termo.
+
+def serie_fibonacci(n):
+    a = 1
+    b = 1 
+    print(a, end=' ') # imprime o primeiro número da série
+    for _ in range(n - 1):
+        a, b = b, a + b # atualiza os valores de a e b
+        print(a, end=' ') # imprime o próximo número da série
+
+numero = 9 # o usuário deseja gerar a série até o 10-ésimo termo
+serie_fibonacci(numero)
+
