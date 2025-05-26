2f3fd818b4dc01a9aee7211e39842e64882462f4
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2025-05-26 10:31:16 -0300

    Curso em video - Aula 04

---
+++ CursoEmVideo/dasafio-02.py
@@ -0,0 +1,7 @@
+print('|| Acesse suas informações de data de nascimento aqui ||')
+nome = input('Qual o seu nome? ')
+dia = int(input('Informe o seu dia de nascimento: ' ))
+mes = input('Informe o seu mes de nascimento: ' )
+ano = int(input('Informe o seu ano de nascimento: ' ))
+
+print(f'{nome} a sua data de nascimento é {dia}/{mes}/{ano}, correto?')
---
+++ CursoEmVideo/desafio-03.py
@@ -0,0 +1,6 @@
+print('============== DESAFIO 3 ==============')
+
+a = int(input('Informe o primeiro número: '))
+b = int(input('Informe o segundo número: '))
+
+print(f'A soma de {a} + {b} é igual a {a+b}')
