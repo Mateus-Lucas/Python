5236f6ce7596d1572db27e3f9fc4046a55b55b26
Mateus Lucas <mateus.lucas.ferreira.07@gmail.com>
2025-10-07 14:18:55 -0300

    Desafio 36 compleo + aula 12

---
+++ CursoEmVideo/cores_terminal.py
@@ -0,0 +1,10 @@
+a = 3
+b = 5
+print(f'Os valores são \33[32;44m{a} e \33[32;44m{b}\33[m')
+
+# print('\033[0;30;41mTESTE\033[m') # Fonte preta com fundo vermelho
+# print('\033[4;33;44mTESTE\033[m') # Fonte amarela e sublinhada com fundo azul
+# print('\033[1;35;43mTESTE\033[m') # Fonte roxa (magenta) em negrito com fundo amarelo
+# print('\033[30;42mTESTE\033[m')   # Fonte preta com fundo verde
+# print('\033[mTESTE\033[m')        # Texto padrão do terminal
+# print('\033[7;30mTESTE\033[m')    # Inversão de cores: fundo branco e fonte preta
--- CursoEmVideo/desafio-36.py
+++ CursoEmVideo/desafio-36.py
@@ -1,10 +1,32 @@
-a = 3
-b = 5
-print(f'Os valores são \33[32;44m{a} e \33[32;44m{b}\33[m')
+print('=' * 20, '| Desafio 20 |', '=' * 20)
 
-# print('\033[0;30;41mTESTE\033[m') # Fonte preta com fundo vermelho
-# print('\033[4;33;44mTESTE\033[m') # Fonte amarela e sublinhada com fundo azul
-# print('\033[1;35;43mTESTE\033[m') # Fonte roxa (magenta) em negrito com fundo amarelo
-# print('\033[30;42mTESTE\033[m')   # Fonte preta com fundo verde
-# print('\033[mTESTE\033[m')        # Texto padrão do terminal
-# print('\033[7;30mTESTE\033[m')    # Inversão de cores: fundo branco e fonte preta
+"""
+Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+O programa vai perguntar o valor da casa, o salário do comprador e em 
+quantos anos ele vai pagar.
+
+Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário 
+ou então o empréstimo será negado.
+"""
+
+print('-=' * 28)
+print(' ' * 17, '\033[1;33mEMPRÉSTIMO BANCÁRIO\033[m')
+print('-=' * 28)
+
+valor_casa = float(input('Informe o valor da casa: R$'))
+salario = float(input('Informe o seu salário: R$'))
+anos_parcela = int(input('Informe em quantos anos você vai pagar: '))
+
+# Cálculo correto da prestação mensal
+prestacao_mensal = valor_casa / (anos_parcela * 12)
+
+# Cálculo da porcentagem da prestação em relação ao salário
+porcentagem_prestacao = (prestacao_mensal / salario) * 100
+
+print(f'\nA prestação mensal será de R${prestacao_mensal:.2f}, equivalente a {porcentagem_prestacao:.2f}% do seu salário.')
+
+if prestacao_mensal > salario * 0.3:
+    print('\033[1;31mEmpréstimo negado!\033[m')
+    print(f'O valor da prestação (R${prestacao_mensal:.2f}) excede 30% do seu salário.')
+else:
+    print('\033[1;32mEmpréstimo aprovado! Parabéns!\033[m')
