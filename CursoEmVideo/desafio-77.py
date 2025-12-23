917820686c3ad5ecc5b34af0593d9b58d2721044
Mateus Lucas <mateus.lucas.ferreira.07@gmail.com>
2025-12-23 11:21:48 -0300

    Desafio 77 completo

---
+++ CursoEmVideo/desafio-77.py
@@ -0,0 +1,12 @@
+print('\033[1;33m' + '=' * 20, '| Desafio 75 |', '=' * 20 + '\033[m')
+
+''' Crie um programa que tenha uma tupla con várias palavras (não usar acentos). Depois disso,
+você deve mostrar, para cada palavra, quais são as suas vogais'''
+
+palavras = ('feijao', 'doce', 'carro', 'teclado', 'livro', 'Estojo')
+
+for palavra in palavras:
+    print(f'\nNa palavra {palavra.upper()} temos: ', end='')
+    for letra in palavra:
+        if letra in 'aeiouAEIOU':
+            print(letra, end=' ')
